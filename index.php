26f85697b85acfb7653de2c481982146faa50d25
u-massy <ushi@3.14159.jp>
2023-06-21 21:31:12 +0900

    change index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+header( 'Cache-Control: post-check=0, pre-check=0', FALSE );
+header('Pragma:no-cache');
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
